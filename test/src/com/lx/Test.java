a32baa040d3ae072612d4e1605e7fb87c66faef8
gazure <172997549@qq.com>
2020-04-21 22:25:19 +0800

    005

---
+++ test/src/com/lx/Test.java
@@ -0,0 +1,16 @@
+package com.lx;
+
+/**
+ * @ProjectName: test
+ * @Package: com.lx
+ * @ClassName: Test
+ * @Author: 郭亚东
+ * @Description: 测试
+ * @Date: 2020/4/21 16:43
+ * @Version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.err.println("Hello World000");
+    }
+}
