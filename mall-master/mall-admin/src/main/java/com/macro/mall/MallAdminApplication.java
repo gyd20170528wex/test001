7ffc43553234d7c1406a48af895c16ce1e63cb28
gazure <172997549@qq.com>
2020-04-22 10:59:39 +0800

    测试01

--- mall-master/mall-admin/src/main/java/com/macro/mall/MallAdminApplication.java
+++ mall-master/mall-admin/src/main/java/com/macro/mall/MallAdminApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 应用启动入口
  * Created by macro on 2018/4/26.
  */
+//启动项目
 @SpringBootApplication
 public class MallAdminApplication {
     public static void main(String[] args) {
